e291767a3db16149c7e7c7bec76089958e93454e
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2021-12-20 13:26:28 +0100

    Registering EventLogger and PlayerQuitOrJoinEventHandler when GamePlugin is enabled

--- src/main/java/fr/pederobien/minecraft/game/GamePlugin.java
+++ src/main/java/fr/pederobien/minecraft/game/GamePlugin.java
@@ -4,16 +4,32 @@ import java.io.FileNotFoundException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import fr.pederobien.dictionary.impl.JarXmlDictionaryParser;
 import fr.pederobien.minecraft.dictionary.impl.MinecraftDictionaryContext;
+import fr.pederobien.minecraft.game.impl.PlayerQuitOrJoinEventHandler;
+import fr.pederobien.utils.event.EventLogger;
 
 public class GamePlugin extends JavaPlugin {
 	private static final Path DICTIONARY_FOLDER = Paths.get("resources/dictionaries");
 
+	private static Plugin instance;
+
+	/**
+	 * @return The instance of this plugin.
+	 */
+	public static Plugin instance() {
+		return instance;
+	}
+
 	@Override
 	public void onEnable() {
+		instance = this;
+		EventLogger.instance().register();
+		PlayerQuitOrJoinEventHandler.instance().register(this);
+
 		try {
 			JarXmlDictionaryParser dictionaryParser = new JarXmlDictionaryParser(getFile().toPath());
 
